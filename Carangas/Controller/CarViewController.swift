9d294dd7ac4936d2539435e9e04c517b19260adb
LuizRamos <luizramospe@hotmail.com>
2021-05-14 20:56:57 -0300

    Refactor the code on  viewWillApper for the new method configScreen Create the method showCarOnWebKit for presenting the WebKit with some images of the car Create extension of CarViewController for implementing the WKNavigationDelegate and KUIDelegate for handling with WebKit View

--- Carangas/Controller/CarViewController.swift
+++ Carangas/Controller/CarViewController.swift
@@ -16,25 +16,56 @@ class CarViewController: UIViewController {
     @IBOutlet weak var aivLoading: UIActivityIndicatorView!
     @IBOutlet weak var webView: WKWebView!
 
-    // MARK: - Super Methods
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        configScreen()
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as? AddEditViewController
+        vc?.car = car
+    }
+
+    // MARK: - Config View Methods
+    fileprivate func configScreen() {
         title = car.name
         lbBrand.text = car.brand
         lbGasType.text = car.gas
         lbPrice.text = "\(car.price)"
+        showCarOnWebKit()
+    }
+    fileprivate func showCarOnWebKit() {
+
+                let name = (car.name + "+" + car.brand).replacingOccurrences(of: " ", with: "+")
+                let urlString = "https://www.google.com.br/search?q=\(name)&tbm=isch"
+                let url = URL(string: urlString)!
+                let request = URLRequest(url: url)
+
+                // permite usar usar gestos para navegar
+                webView.allowsBackForwardNavigationGestures = true
+                webView.allowsLinkPreview = true // preview usando 3D touch
+                webView.navigationDelegate = self
+                webView.uiDelegate = self
+                webView.load(request)
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           let vc = segue.destination as? AddEditViewController
-           vc?.car = car
-       }
+} //end CarViewController
 
-    
+//MARK: - Extensions
+extension CarViewController: WKNavigationDelegate, WKUIDelegate {
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("stopLoading")
+        aivLoading.stopAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        aivLoading.stopAnimating()
+    }
 
 }
